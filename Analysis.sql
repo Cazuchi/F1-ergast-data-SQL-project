311e24adb6c2d8d736eee7e5decfb34d67fb64af
Mike <mikeehansen@gmail.com>
2026-03-15 19:55:23 +0100

    Added time data.

--- Analysis.sql
+++ Analysis.sql
@@ -280,9 +280,11 @@ final_output_table_filtered AS (
         END AS "Driver #2 stint volatility trend",
         ABS(fot."Driver #1 rolling STDDEV" - fot."Driver #2 rolling STDDEV") AS "ABS difference between driver STDDEVs",
         fot."Race ID",
+        rc.raceyear as "Year",
         fot."G&L distance measure",
         fot."Team/Drivers combo race counter"
     FROM final_output_table fot
+    LEFT JOIN races rc ON fot."Race ID" = rc.raceid
     WHERE fot."Team/Drivers combo race counter" >= 10
     WINDOW 
         stint_window_driver_1 AS (
@@ -304,7 +306,7 @@ FROM final_output_table_filtered
 WHERE "Driver #1 name" = 'barbazza' AND "Driver #2 name" = 'barbazza'
 ORDER BY "Race ID";
 
-SELECT * FROM results;
+SELECT * FROM races;
 
 
 /*
